be23eb3537808163dbf93d045d6dfe65b1b3c240
badangle520 <24562323+badangle520@users.noreply.github.com>
2020-09-12 14:03:57 +0800

    Update 20200910 事务.sql

--- 20200910 事务.sql
+++ 20200910 事务.sql
@@ -120,10 +120,24 @@ ROLLBACK TO a;#回滚到保存点
 SELECT * FROM account;
 
 
+########################################################################################################
+#                     测试 -- 开始
+########################################################################################################
+#1.向表emp2的id列中添加PRIMARY KEY约束（my_emp_id_pk）
 
+ALTER TABLE emp2 MODIFY COLUMN id INT PRIMARY KEY;
+ALTER TABLE emp2 ADD CONSTRAINT my_emp_id_pk PRIMARY KEY(id);
 
+#2.	向表dept2的id列中添加PRIMARY KEY约束（my_dept_id_pk）
 
+#3.	向表emp2中添加列dept_id，并在其中定义FOREIGN KEY约束，与之相关联的列是dept2表中的id列。
+ALTER TABLE emp2 ADD COLUMN dept_id INT;
+ALTER TABLE emp2 ADD CONSTRAINT fk_emp2_dept2 FOREIGN KEY(dept_id) REFERENCES dept2(id);
 
-
-
+		位置		支持的约束类型			是否可以起约束名
+列级约束：	列的后面	语法都支持，但外键没有效果	不可以
+表级约束：	所有列的下面	默认和非空不支持，其他支持	可以（主键没有效果）
+########################################################################################################
+#                     测试 -- 结束
+########################################################################################################
 
